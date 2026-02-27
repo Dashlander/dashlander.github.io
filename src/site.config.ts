a62308a804d6bfb5758fbd3ded7c6510d1bc1f57
dashlander <nishanttatar@gmail.com>
2026-02-27 20:22:17 +0530

    chore: site config locales changed

--- src/site.config.ts
+++ src/site.config.ts
@@ -6,7 +6,7 @@ export const siteConfig: SiteConfig = {
 	author: "Nishant Tatar",
 	// Date.prototype.toLocaleDateString() parameters, found in src/utils/date.ts.
 	date: {
-		locale: "en-GB",
+		locale: "en-IN",
 		options: {
 			day: "numeric",
 			month: "short",
@@ -14,11 +14,11 @@ export const siteConfig: SiteConfig = {
 		},
 	},
 	// Used as the default description meta property and webmanifest description
-	description: "An opinionated starter theme for Astro",
+	description: "A personal website",
 	// HTML lang property, found in src/layouts/Base.astro L:18 & astro.config.ts L:48
-	lang: "en-GB",
+	lang: "en-IN",
 	// Meta property, found in src/components/BaseHead.astro L:42
-	ogLocale: "en_GB",
+	ogLocale: "en_IN",
 	/* 
 		- Used to construct the meta title property found in src/components/BaseHead.astro L:11 
 		- The webmanifest name found in astro.config.ts L:42
@@ -26,7 +26,6 @@ export const siteConfig: SiteConfig = {
 		- In the footer found in src/components/layout/Footer.astro L:12
 	*/
 	title: "Dashlander",
-	// ! Please remember to replace the following site property with your own domain, used in astro.config.ts
 	url: "https://dashlander.github.io/",
 };
 
